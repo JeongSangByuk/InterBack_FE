b9da31e0cebe8169c7e6f4178f5e6a5f127beee9
jsb100800@naver.com <jsb100800@naver.com>
2022-04-04 02:03:42 +0900

    vue_test

---
+++ vue_test/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div>
+    <HeaderView></HeaderView>
+  </div>
+</template>
+
+<script>
+
+import HeaderView from './components/layout/HeaderView.vue';
+
+export default {
+  name: 'App',
+
+  data(){
+    return {
+
+    }
+  },
+
+  methods : {
+
+  },
+
+  components: {
+    HeaderView
+  },
+}
+</script>
+
+<style>
+#app {
+}
+</style>
