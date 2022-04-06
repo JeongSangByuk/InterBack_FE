6b39bd8c57d7a5d6ee484bf95ef2d261a1383c9e
JeongSangByuk <jsb100800@naver.com>
2022-04-06 16:32:01 +0900

    [ADD] interview room video frame

--- vue_test/src/views/2_pages/InterviewRoom.vue
+++ vue_test/src/views/2_pages/InterviewRoom.vue
@@ -3,19 +3,64 @@
     <div class="video-container">
       <p class="video-container__title">카카오 BE 그룹3 면접장</p>
       <div class="interviwer-container">
-        <div class="test"></div>
-        <div class="test"></div>
-        <div class="test"></div>
-        <div class="test"></div>
+        <div class="interviewer">
+          <p class="interviewer__name">면접관1. 정상벽</p>
+          <div class="interviewer__video">
+            <img src="img/img_user_interview.png" />
+          </div>
+        </div>
+        <div class="interviewer">
+          <p class="interviewer__name">면접관2. 박태순</p>
+          <div class="interviewer__video">
+            <img src="img/img_user_interview.png" />
+          </div>
+        </div>
+        <div class="interviewer">
+          <p class="interviewer__name">면접관3. 김채운</p>
+          <div class="interviewer__video">
+            <img src="img/img_user_interview.png" />
+          </div>
+        </div>
+        <div class="interviewer">
+          <p class="interviewer__name">면접관4. 박윤굥</p>
+          <div class="interviewer__video">
+            <img src="img/img_user_interview.png" />
+          </div>
+        </div>
       </div>
-      <div class="interviwer-container">
-        <div class="test"></div>
-        <div class="test"></div>
-        <div class="test"></div>
-        <div class="test"></div>
+      <div class="interviwee-container">
+        <div class="interviewee"></div>
+        <div class="interviewee"></div>
+        <div class="interviewee"></div>
+        <div class="interviewee"></div>
       </div>
     </div>
-    <div class="text-record-container">qwe</div>
+    <div class="text-record-container">
+      <p class="text-record-container__title">실시간 회의록</p>
+      <div class="text-record-list">
+        <div class="chat">
+          <p class="chat__name">정상벽벽</p>
+          <p class="chat__contents">안녕하세요.요.</p>
+        </div>
+        <div class="chat">
+          <p class="chat__name">정상벽벽</p>
+          <p class="chat__contents">안녕하세요.요.</p>
+        </div>
+        <div class="chat">
+          <p class="chat__name">정상벽벽</p>
+          <p class="chat__contents">안녕하세요.요.</p>
+        </div>
+
+        <div class="chat">
+          <p class="chat__name">정상벽벽</p>
+          <p class="chat__contents">안녕하세요.요.</p>
+        </div>
+        <div class="chat">
+          <p class="chat__name">정상벽벽</p>
+          <p class="chat__contents">안녕하세요.요.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
